41fcf903a89fe830a29d2c9f9d43b4f6aa48a693
Diodorov Danil <ddldiodorov@gmail.com>
2019-10-15 15:26:32 +0900

    add

---
+++ if-03.js
@@ -0,0 +1,9 @@
+//TODO: Впишите имя функции из задания вместо ...
+//TODO: Добавьте правильные параметры
+//TODO: Напишите функцию
+
+function f(a, b, c, d){
+	return Math.min(Math.max(a, b), Math.max(c, d));
+}
+
+module.exports = f;
