0e26b8bfde7a96aa524e53848d72a370e1e631db
SadScream <sadscream.ilia@gmail.com>
2019-10-21 23:18:41 +0900

    done

--- if-06.js
+++ if-06.js
@@ -1,8 +1,15 @@
-// npm install
-// npm test
+function f(k, b, R) {
+	D = 4*k*k*b*b - 4*(1+k*k)*(b*b-R*R)
 
-function f(k, b, R){
-	return null
+	if (D < 0) {
+		return 0
+	}
+	else if (D > 0) {
+		return 2
+	}
+	else if (D === 0) {
+		return 1
+	}
 }
 
 module.exports = f;
