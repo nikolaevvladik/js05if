f3287e0346c57286d9314d8f1ee289e9a2414076
SadScream <sadscream.ilia@gmail.com>
2019-11-04 19:28:29 +0900

    replaced Math to m

--- if-13.js
+++ if-13.js
@@ -1,22 +1,22 @@
 var m = Math
 
 function S(x1, y1, x2, y2, x3, y3){
-	var a = Math.sqrt(m.pow(x2-x1, 2) + m.pow(y2-y1, 2))
-	var b = Math.sqrt(m.pow(x3-x2, 2) + m.pow(y3-y2, 2))
-	var c = Math.sqrt(m.pow(x3-x1, 2) + m.pow(y3-y1, 2))
+	var a = m.sqrt(m.pow(x2-x1, 2) + m.pow(y2-y1, 2))
+	var b = m.sqrt(m.pow(x3-x2, 2) + m.pow(y3-y2, 2))
+	var c = m.sqrt(m.pow(x3-x1, 2) + m.pow(y3-y1, 2))
 	var d = 0
 
 	if (a == b) {
 		d = m.sqrt(a*a+b*b)
 
-		if (d = c*Math.sqrt(2)) {
+		if (d = c*m.sqrt(2)) {
 			return true
 		}
 	}
 	else if (a == c) {
 		d = m.sqrt(a*a+c*c)
 
-		if (d = b*Math.sqrt(2)) {
+		if (d = b*m.sqrt(2)) {
 			return true
 		}
 	}
