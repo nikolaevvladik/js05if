338a765f3a945e8f0976c309a56aae5fb7c32063
Николаев <artbudikin@gmail.com>
2019-11-05 10:57:55 +0900

    работа 1

--- if-x3.js
+++ if-x3.js
@@ -2,9 +2,52 @@
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
 
-function ...(){
+function MonthName(n){
+	
+	switch(n) {
+		case 1:
+		  name='январь'
+		  break;
+		case 2:
+		  name='февраль'
+		  break;
+		case 3:
+		  name='март'
+		  break;
+		case 4:
+		  name='апрель'
+		  break;
+		case 5:
+		  name='май'
+		  break;
+		case 6:
+		  name='июнь'
+		  break;
+		case 7:
+		  name='июль'
+		  break;
+		case 8:
+		  name='август'
+		  break;
+		case 9:
+		  name='сентябрь'
+		  break;
+		case 10:
+		  name='октябрь'
+		  break;
+		case 11:
+		  name='ноябрь'
+		  break;
+		case 12:
+		  name='декабрь'
+		  break;
+		default:return'ошибка!'
+	}
+
+return name;
+	}
 
 
-}
 
-module.exports = ...;
+
+module.exports = MonthName;
--- if-x4.js
+++ if-x4.js
@@ -1,10 +1,23 @@
 //TODO: Впишите имя функции из задания вместо ...
 //TODO: Добавьте правильные параметры
 //TODO: Напишите функцию
+//04 f (a,b,c,d) = max ( min (|a|,|b|), max (|c|,|d|)) 
 
-function ...(){
+function f(a, b, c, d) {
+    if (Math.abs(a) < Math.abs(b))
+    { 
+        min = Math.abs(a);}
+    else {min = Math.abs(b)
+            }
+    if (Math.abs(c) > Math.abs(d))
+    {
+        max = Math.abs(c);}
+    else {max = Math.abs(d)
+            }
+    if (min < max)
+        f = max;
+    else f = min;
 
-
+    return f;
 }
-
-module.exports = ...;
+    module.exports = f;
